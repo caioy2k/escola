0fb51dc22bb7b7aa83210fe44b0a40854d91b5a1
caioy2k <caio.araujolacerda10@gmail.com>
2025-08-13 17:20:30 -0300

    teste

--- src/main/java/com/etec/escola/Controller/AlunoController.java
+++ src/main/java/com/etec/escola/Controller/AlunoController.java
@@ -1,4 +1,11 @@
 package com.etec.escola.Controller;
 
+import com.etec.escola.Service.AlunoService;
+
 public class AlunoController {
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
 }
