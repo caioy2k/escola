f05b9a871af81cdebf939242d40ed3dc48eeab75
caioy2k <caio.araujolacerda10@gmail.com>
2025-08-20 17:15:58 -0300

    .

--- src/main/java/com/etec/escola/Controller/AlunoController.java
+++ src/main/java/com/etec/escola/Controller/AlunoController.java
@@ -1,6 +1,13 @@
 package com.etec.escola.Controller;
 
+import com.etec.escola.Models.Aluno;
 import com.etec.escola.Service.AlunoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/aluno")
 
 public class AlunoController {
     private final AlunoService alunoService;
@@ -8,4 +15,25 @@ public class AlunoController {
     public AlunoController(AlunoService alunoService) {
         this.alunoService = alunoService;
     }
+    @GetMapping
+    public List<Aluno> listaDeAlunos() {
+        return alunoService.buscarAlunos();
+    }
+
+    @PostMapping
+    public Aluno salvarNovoAluno(@RequestBody Aluno dadosAluno){
+        return alunoService.salvarAluno(dadosAluno);
+    }
+
+    @GetMapping("/{id}")
+    public Aluno buscarAlunoId(@PathVariable Long id){
+        return alunoService.buscarAlunoId(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void excluirAluno(@PathVariable Long id){
+        alunoService.deletarAluno(id);
+    }
+
 }
+
